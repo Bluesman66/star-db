6503b25fef4d88b68efd4956cf646d64749d0c34
Bluesman66 <alex.paclin@gmail.com>
2019-05-12 18:26:18 -0400

    053 Обработка ошибок в Fetch API

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 const getResource = async (url) => {
     const res = await fetch(url);
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, received ${res.status}`);
+    }
     const body = await res.json();
     return body;
 };
 
-getResource('https://swapi.co/api/people/1/')
+getResource('https://swapi.co/api/people/1232323/')
     .then((body) => {
         console.log(body);
     })
-
-// fetch('https://swapi.co/api/people/1/')
-//     .then((res) => {
-//         return res.json();
-//     })
-//     .then((body) => {
-//         console.log(body);
-//     });
+    .catch((err) => {
+        console.error(err);
+    })
