d50e7bd765aab7d3b13dc15022d5bd333f3b6e96
alex.paclin <alex.paclin@gmail.com>
2019-05-19 16:45:32 +0500

    067 Используем на практике componentDidMount()

--- src/components/person-details/person-details.js
+++ src/components/person-details/person-details.js
@@ -1,29 +1,67 @@
 import React, { Component } from 'react';
 
 import './person-details.css';
+import SwapiService from '../../services/swapi-service';
 
 export default class PersonDetails extends Component {
 
+    swapiService = new SwapiService();
+
+    state = {
+        person: null
+    }
+
+    componentDidMount = () => {
+        this.updatePerson();
+    }
+
+    componentDidUpdate = (prevProps) => {
+        if (this.props.personId !== prevProps.personId) {
+            this.updatePerson();
+        }
+    }
+
+    updatePerson = () => {
+        const { personId } = this.props;
+        
+        if (!personId) {
+            return;
+        }
+
+        this.swapiService
+            .getPerson(personId)
+            .then((person) => {
+                this.setState({ person })
+            });
+    }    
+
     render() {
+
+        if (!this.state.person) {
+            return <span>Select a person from a list</span>;
+        }
+
+        const { person: { id, name, gender, birthYear, eyeColor } } = this.state;
+        
         return (
             <div className="person-details card">
                 <img className="person-details__image"
-                    src="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
+                    src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} />
 
                 <div className="person-details__body card-body">
-                    <h4>R2-D2</h4>
+                    <h4>{name}</h4>
                     <ul className="person-details__list list-group list-group-flush">
                         <li className="person-details__item list-group-item">
                             <span className="person-details__term">Gender</span>
-                            <span>male</span>
+                            <span>{gender}</span>
                         </li>
                         <li className="person-details__item list-group-item">
                             <span className="person-details__term">Birth Year</span>
-                            <span>43</span>
+                            <span>{birthYear}</span>
                         </li>
                         <li className="person-details__item list-group-item">
                             <span className="person-details__term">Eye Color</span>
-                            <span>red</span>
+                            <span>{eyeColor}</span>
                         </li>
                     </ul>
                 </div>
