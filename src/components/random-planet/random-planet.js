14b192e5be11283433b51040913f96c7f4ab3194
alex.paclin <alex.paclin@gmail.com>
2019-08-02 09:38:37 +0500

    090 Свойство defaultProps

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -17,8 +17,9 @@ export default class RandomPlanet extends Component {
     }    
 
     componentDidMount = () => {
+        const { updateInterval } = this.props;
         this.updatePlanet();
-        this.interval = setInterval(this.updatePlanet, 10000);        
+        this.interval = setInterval(this.updatePlanet, updateInterval);        
     }
 
     componentWillUnmount = () => {
@@ -65,6 +66,10 @@ export default class RandomPlanet extends Component {
             </div>
         );
     }
+}
+
+RandomPlanet.defaultProps = {
+    updateInterval: 10000
 }
 
 const PlanetView = ({ planet }) => {
